de1a47dc035958b45bfb439d377aad06192925ff
Samar Anwar <sanwar3@id.iit.edu>
2020-12-05 11:04:01 -0600

    formatting

--- assets/js/scenes/preloader.js
+++ assets/js/scenes/preloader.js
@@ -6,17 +6,17 @@ export default class Preloader extends Phaser.Scene {
   preload() {
     this.load.image('background', 'assets/img/background.png');
 
-
     this.load.image(
       'tiles',
       // 'assets/tiles/interaction-element.png',
-      'assets/tiles/frame.png');
+      'assets/tiles/frame.png'
+    );
 
     this.load.tilemapTiledJSON(
       'mygame',
       // 'assets/tiles/interaction-element.json',
       'assets/tiles/level-design.json'
-      );
+    );
 
     this.load.atlas(
       'character',
