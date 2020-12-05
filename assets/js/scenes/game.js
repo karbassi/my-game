93ada060d9ad05442fdf997f420a790b452eda3a
Ali Karbassi <ali@karbassi.com>
2020-12-05 13:13:00 -0600

    Saturday work

--- assets/js/scenes/game.js
+++ assets/js/scenes/game.js
@@ -13,29 +13,40 @@ export default class Game extends Phaser.Scene {
   }
 
   create() {
-    this.add.image(900, 1400, 'background');
     createCharacterAnims(this.anims);
 
+    this.add.image(900, 1400, 'background');
+
     const map = this.make.tilemap({ key: 'mygame' });
+    const tileset = map.addTilesetImage('entire-set', 'tiles');
 
-    // const tileset = map.addTilesetImage('interaction-element', 'interaction-tiles');
-    const tileset = map.addTilesetImage('entire-set-2', 'tiles');
+    this.mainLayer = map.createStaticLayer('main', tileset);
+    this.mainLayer.setCollisionByProperty({ collides: true });
+    this.interactionLayer = map.createStaticLayer('interaction-layer', tileset);
+    this.interactionLayer.setCollisionByProperty({ pickup: true });
 
-    // this.main = map.createStaticLayer('interaction-layer', tileset);
-    this.main = map.createStaticLayer('main', tileset);
+    // debugDraw(this.main, this);
 
-    this.main.setCollisionByProperty({ collides: true });
-
-    debugDraw(this.main, this);
-
-    this.character = this.physics.add.sprite(600, 1100, 'character');
-    this.character.play('character-idle');
-
-    this.physics.add.collider(this.character, this.main);
-
+    this.character = this.createCharacter();
+    this.physics.add.collider(this.character, this.mainLayer);
+    this.physics.add.collider(
+      this.character,
+      this.interactionLayer,
+      this.pickupInteraction
+    );
     this.cameras.main.startFollow(this.character, true);
 
-    this.character.setVelocity(0, 0);
+    //this.character.setVelocity(0, 0);
+  }
+
+  createCharacter() {
+    const character = this.physics.add.sprite(600, 1100, 'character');
+    character.setActive(true);
+    // character.setCollideWorldBounds(true);
+    character.onWorldBounds = true;
+    character.play('character-idle');
+
+    return character;
   }
 
   update(time, deltatime) {
@@ -43,28 +54,33 @@ export default class Game extends Phaser.Scene {
       return;
     }
 
+    // console.log(this.character.body.blocked.down);
+
     if (this.cursors.left.isDown) {
       this.character.play('character-run-left', true);
       this.character.setVelocityX(-this.CHARACTER_SPEED);
     } else if (this.cursors.right.isDown) {
       this.character.play('character-run-right', true);
       this.character.setVelocityX(this.CHARACTER_SPEED);
-      // } else if (this.cursors.up.isDown) {
-      //   this.character.play('character-run-right', true);
-      //   this.character.setVelocityY(-330);
     } else {
       this.character.play('character-idle', true);
       this.character.setVelocityX(0);
     }
 
+    if (this.cursors.up.isDown && this.character.body.blocked.down) {
+      this.character.play('character-run-right', true);
+      this.character.setVelocityY(-this.CHARACTER_SPEED * 2);
+    }
     // if (this.cursors.up.isDown) {
     //   this.character.setVelocityY(-330);
     // }
 
     //   if (cursors.up.isDown && player.body.touching.down) { player.setVelocityY(-330);}
   }
-}
 
-// function collectInteraction-element(character, interaction-element) {
-//   interaction-element.disableBody(true, true)
-// }
+  pickupInteraction(character, interaction) {
+    // console.log(interaction);
+    // interaction.destroy();
+    // interaction.visible = false;
+  }
+}
